a3d15c4cb3f8c362ff7b78ffa12070a6bccd6f9e
paola <pebitaa5795@hotmail.com>
2023-07-07 22:52:59 -0600

    indice de java

---
+++ script.js
@@ -0,0 +1,61 @@
+let objeto = 'estufa de casa';
+console.log(objeto.length);
+
+console.log(objeto.includes('estufa'));
+
+console.log(objeto.indexOf('casa'));
+
+// Booleano
+let variableBooleana1 = false;
+console.log(variableBooleana1);
+
+let variableBooleana2 = true;
+console.log(variableBooleana2);
+
+// Cadenas de texto
+let texto = 'Hola mundo';
+console.log(texto);
+
+let texto2 = 'Casa Bonita';
+console.log(texto2);
+
+
+// Numérico
+let numero = 57;
+console.log(numero);
+
+let numero2 = 95;
+console.log(numero2);
+
+//operacion matematica
+let numero1=10;
+numero +=5;
+document.write(numero1);
+
+// Objetos
+const persona = { nombre: 'Leslie', edad: 24 };
+console.log(persona);
+
+const persona2 = { color: 'azul', tamaño: 'grande' };
+console.log(persona2);
+
+//nulos
+let Null1 = null;
+console.log(Null1);
+
+let Null2 = null;
+console.log(Null2);
+
+//concatenacion
+let nombre1='Paola ', apellido1='Bartolon';
+console.log(nombre1+apellido1);
+
+
+//operadores de comparacion
+let numeros=23;
+let num=13;
+document.write(numeros > num)
+
+let numeros1=23;
+let num1=13;
+document.write(numeros1 < num1)
